f8ba9d7d80b6bcfd5f77604be6b5926003303d0c
TownsJ <noaj.t.93@gmail.com>
2024-05-28 10:49:32 -0400

    Implement "Add new tasks to list" feature

--- web.py
+++ web.py
@@ -3,6 +3,13 @@ import functions
 
 tasks = functions.get_tasks()
 
+
+def add_task():
+    task = st.session_state["new_task"] + "\n"
+    tasks.append(task)
+    functions.write_tasks(tasks)
+
+
 st.title("My To-do List App")
 st.write("This app is made to increase your productivity")
 
@@ -10,6 +17,6 @@ st.write("This app is made to increase your productivity")
 for task in tasks:
     st.checkbox(task)
 
-st.text_input(label="XX",
-              label_visibility='hidden',
-              placeholder="Enter tasks here...")
+st.text_input(label="XX", label_visibility='hidden',
+              placeholder="Enter tasks here...",
+              on_change=add_task, key="new_task")
